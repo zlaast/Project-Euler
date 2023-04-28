e91cac87eed05df14877e7a17f071516768ebb05
Z'Laast Allicock <allicockz@gmail.com>
2023-04-27 23:18:26 -0400

    Update problem04.py

--- Problems [1-100)/problem04.py
+++ Problems [1-100)/problem04.py
@@ -2,14 +2,13 @@
 # Largest Palindrome Product
 
 max = 0
-product = 0
+
 for num1 in range(100, 1000):
-    for num2 in range(100, 1000):
+    for num2 in range(num1, 1000):
         product = num1*num2
 
         if (str(product) == str(product)[::-1]):
             if (product > max):
                 max = product
-print(max)
 
-        
+print(max)
