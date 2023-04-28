ba9de353e39d3d9ed858ee69fb90d5b186a81e2b
Z'Laast Allicock <allicockz@gmail.com>
2023-04-28 11:47:07 -0400

    Update problem05.py

--- Problems [1-100)/problem05.py
+++ Problems [1-100)/problem05.py
@@ -2,69 +2,25 @@
 # Smallest Multiple
 
 import math
-from PEF import soe     # This was written by me
 
-max_product = 20
-primes = soe(max_product)
-
+primes = [2, 3, 5, 7, 11, 13, 17, 19]
 factors = []
-for num in range(2, max_product+1):
-    ls = []
-    # Perform prime factorization
+
+for num in range(2, 21):
+    list = []
+
+    # Step 1: Perform prime factorization on each number
     for prime in primes:
         while (num % prime == 0):
-            num /= prime
-            ls.append(prime)
-    
-    # Discard duplicates from the list
-    num_set = set(ls)
+            num = num / prime
+            list.append(prime)
 
-    # Discard factorizations with different numbers
-    if len(num_set) == 1:
-        factors.append(num_set.pop())
+    # Step 2: Create a set, discarding duplicate numbers
+    uniq_set = set(list)
 
+    # Step 3: Discard factorizations with different numbers
+    if len(uniq_set) == 1:
+        factors.append(uniq_set.pop())
+
+# Step 4: Multiply all factors
 print(math.prod(factors))
-
-
-'''
-How this script works:
-
-Using the example of 2520, we perform prime factorization
-2520 = 2*2*2*3*3*5*7
-
-If we do prime factorization of the numbers from 1-10 we get:
- 1 = 1
- 2 = 2
- 3 = 3
- 4 = 2*2
- 5 = 5
- 6 = 2*3
- 7 = 7
- 8 = 2*2*2
- 9 = 3*3
-10 = 2*5
-
-If we discard "1" and any factorizations with different numbers in them (6 and 10), we get:
-2 = 2
-3 = 3
-4 = 2*2
-5 = 5
-7 = 7
-8 = 2*2*2
-9 = 3*3
-
-If we put the factorizations into sets (which discards duplicate values) we get:
-2 = 2
-3 = 3
-4 = 2
-5 = 5
-7 = 7
-8 = 2
-9 = 3
-
-The result, as you may notice is [2,3,2,5,7,2,3] which when rearranged is [2,2,2,3,3,5,7] which is the prime factorization of 2520!
-
-'''
-
-
-
